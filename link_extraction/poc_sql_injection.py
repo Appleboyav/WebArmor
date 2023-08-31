bd44565a74fd81b498c075cbb9faf6908f429fb9
Appleboyav0 <avior2060@gmail.com>
2023-08-31 03:15:30 +0300

    fixed some things

--- link_extraction/poc_sql_injection.py
+++ link_extraction/poc_sql_injection.py
@@ -1,9 +1,5 @@
-import sys
-sys.path.insert(1, r'C:\Users\test0\OneDrive\שולחן העבודה\Magshimim\12th Grade - Third year\link extraction')
-
 import extract_links_with_validation
 import pandas as pd
-
 
 def main():
     links = extract_links_with_validation.get_internal_links()
