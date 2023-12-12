286a2e3b4d90aef76cd8e7d706b0109b14737f16
Appleboyav0 <avior2060@gmail.com>
2023-12-12 02:31:14 +0200

    Version 1.1 sqli

--- Under_Develop/SQLi/injection_prototype.py
+++ Under_Develop/SQLi/injection_prototype.py
@@ -1,7 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
 
 # ! not working
+
 
 # Target URL
 url = "http://127.0.0.1:80/DVWA/login.php"
@@ -13,11 +13,34 @@ payload = {
     "Login": "Login",
 }
 
+# Custom headers
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Cookie": "PHPSESSID=1o4bq0as8krq2ckdatmaesbhhq; security=low",
+    "Host": "localhost",
+    "Origin": "http://localhost",
+    "Referer": "http://localhost/DVWA/login.php",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+    "Sec-GPC": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Brave";v="120"',
+    "sec-ch-ua-mobile": "?0",
+}
+
 # Create a session to persist the login session
 session = requests.Session()
 
-# Make a POST request to the login page with the credentials
-response = session.post(url, data=payload)
+# Make a POST request to the login page with the credentials and custom headers
+response = session.post(url, data=payload, headers=headers)
 
 # Check if login was successful
 if "Login failed" in response.text:
@@ -35,3 +58,4 @@ print(response.text)
 
 # Don't forget to close the session when you're done
 session.close()
+
