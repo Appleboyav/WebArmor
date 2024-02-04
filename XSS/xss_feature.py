6b8d2dc126763e00a38af133ed73d6fa50dcbc7b
Appleboyav0 <avior2060@gmail.com>
2024-02-04 02:26:33 +0200

    Add the use of helper files

--- XSS/xss_feature.py
+++ XSS/xss_feature.py
@@ -1,38 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from Helpers import helper_dvwa
+from Helpers import helper_generic_tags
+
 
 BASE_URL = "http://127.0.0.1:80/DVWA"
-
-VALUES = {
-    'username': 'admin',
-    'password': 'password',
-    'Login':'Login'
-}
-
 SCRIPT = "<script>alert('XSS')</script>"
-
-INJECT_DATA = {"name" : SCRIPT, "submit" : "Submit"}
-
-
-def get_user_token() -> str:
-    with requests.Session() as c:
-        res = c.get(f"{BASE_URL}/login.php")
-        soup = bs(res.text, "html.parser")
-        cookie = soup.find_all("input", {"type": "hidden"})
-        user_token = cookie[0]["value"]
-        return user_token
-    
-
+INJECT_DATA = {"name": SCRIPT, "submit": "Submit"}
 COOKIES_JSON = {
-    "PHPSESSID": get_user_token(),
+    "PHPSESSID": helper_dvwa.get_user_token(f"{BASE_URL}/login.php"),
     "security": "low"
 }
-
-
-def get_forms(html_page) -> list:
-    soup = bs(html_page, "html.parser")
-    forms = soup.find_all("form")
-    return list(forms)
 
 
 def main():
@@ -45,7 +23,7 @@ def main():
     xss_r_page = session.get(f"{BASE_URL}/vulnerabilities/xss_r/", cookies=COOKIES_JSON)
 
     # To see the structure of the data we need to send:
-    ls_forms = get_forms(xss_r_page.text)
+    ls_forms = helper_generic_tags.GetGenericTags.get_tags(xss_r_page.text, "form", {})
 
     # Inserting the data and in put the script in the name filed:
     res = requests.get(f"{BASE_URL}/vulnerabilities/xss_r/", params = INJECT_DATA, cookies = COOKIES_JSON )
@@ -55,8 +33,6 @@ def main():
         print("Your website is vulnerable to XSS Injection attack!")
     else:
         print("Your website is NOT vulnerable to XSS Injection attack.")
-
-
 
 
 if __name__ == "__main__":
