c3f21b35d33a35107e4f567435047f6be4128c7b
Appleboyav0 <avior2060@gmail.com>
2024-02-04 02:38:38 +0200

    used th helper files

--- XSS/xss_feature.py
+++ XSS/xss_feature.py
@@ -13,25 +13,27 @@ COOKIES_JSON = {
 
 
 def main():
-    
-    # Pass the login page: "http://127.0.0.1:80/DVWA/index.php"
-    session = requests.Session()
-    index_page_response = session.get(f"{BASE_URL}/index.php", cookies = COOKIES_JSON)
+    with requests.Session() as sess:
+        sess.cookies.update(COOKIES_JSON)
 
-    # Entering to xss reflected page : "http://127.0.0.1:80/DVWA/vulnerabilities/xss_r"
-    xss_r_page = session.get(f"{BASE_URL}/vulnerabilities/xss_r/", cookies=COOKIES_JSON)
+        # TODO: elad removed this unused lines (20, 27)
+        # Pass the login page: "http://127.0.0.1:80/DVWA/index.php"
+        index_page_response = sess.get(f"{BASE_URL}/index.php")
 
-    # To see the structure of the data we need to send:
-    ls_forms = helper_generic_tags.GetGenericTags.get_tags(xss_r_page.text, "form", {})
+        # Entering to xss reflected page : "http://127.0.0.1:80/DVWA/vulnerabilities/xss_r"
+        xss_r_page = sess.get(f"{BASE_URL}/vulnerabilities/xss_r/")
 
-    # Inserting the data and in put the script in the name filed:
-    res = requests.get(f"{BASE_URL}/vulnerabilities/xss_r/", params = INJECT_DATA, cookies = COOKIES_JSON )
-    
-    # Check i the script saved in the website:
-    if SCRIPT in res.text:
-        print("Your website is vulnerable to XSS Injection attack!")
-    else:
-        print("Your website is NOT vulnerable to XSS Injection attack.")
+        # To see the structure of the data we need to send:
+        ls_forms = helper_generic_tags.GetGenericTags.get_tags(xss_r_page.text, "form", {})
+
+        # Inserting the data and in put the script in the name filed:
+        res = sess.get(f"{BASE_URL}/vulnerabilities/xss_r/", params=INJECT_DATA)
+
+        # Check if the script saved in the website:
+        if SCRIPT in res.text:
+            print("Your website is vulnerable to XSS Injection attack!")
+        else:
+            print("Your website is NOT vulnerable to XSS Injection attack.")
 
 
 if __name__ == "__main__":
--- XSS/xss_feature_general.py
+++ XSS/xss_feature_general.py
@@ -1,69 +1,41 @@
 import requests
-from bs4 import BeautifulSoup as bs
+from Helpers import helper_dvwa
 
 BASE_URL = "http://127.0.0.1:80/DVWA"
-
-VALUES = {
-    'username': 'admin',
-    'password': 'password',
-    'Login':'Login'
-}
-
 SCRIPT = "<script>alert('XSS')</script>"
-
-INJECT_DATA = {"name" : SCRIPT, "submit" : "Submit"}
-
-
-def get_user_token() -> str:
-    with requests.Session() as c:
-        res = c.get(f"{BASE_URL}/login.php")
-        soup = bs(res.text, "html.parser")
-        cookie = soup.find_all("input", {"type": "hidden"})
-        user_token = cookie[0]["value"]
-        return user_token
-    
-
+INJECT_DATA = {"name": SCRIPT, "submit": "Submit"}
 COOKIES_JSON = {
-    "PHPSESSID": get_user_token(),
+    "PHPSESSID": helper_dvwa.get_user_token(f"{BASE_URL}/login.php"),
     "security": "low"
 }
 
 
-def get_forms(html_page) -> list:
-    soup = bs(html_page, "html.parser")
-    forms = soup.find_all("form")
-    return list(forms)
-
-
 def main():
-    
- 
-    session = requests.Session()
+    with requests.Session() as sess:
+        sess.cookies.update(COOKIES_JSON)
 
-    url = input("Enter the URL you want to check for an XSS vulnerability:")
-    print("Enter the structure of the form you want to check for an XSS vulnerability:  ")
-    print("According to the structure of a dictionary, Example: {'name' : ori, 'submit' : 'Submit'}")
-    string_form = input("")
+        url = input("Enter the URL you want to check for an XSS vulnerability:")
+        print("Enter the structure of the form you want to check for an XSS vulnerability:  ")
+        print("According to the structure of a dictionary, Example: {'name' : ori, 'submit' : 'Submit'}")
+        string_form = input("")
 
-    # Get the structure of the form as a dictionary -  change type from string to dictionary:
-    dictionary_form = eval(string_form)
+        # Get the structure of the form as a dictionary -  change type from string to dictionary:
+        dictionary_form = eval(string_form)
 
-    # Get the first key in the dictionary
-    first_key = list(dictionary_form.keys())[0]
+        # Get the first key in the dictionary
+        first_key = list(dictionary_form.keys())[0]
 
-    # Replace the value in the first place in the dictionary we got to the SCRIPT : 
-    dictionary_form[first_key] = SCRIPT
+        # Replace the value in the first place in the dictionary we got to the SCRIPT :
+        dictionary_form[first_key] = SCRIPT
 
-    # Inserting the data and in put the script in the name filed:
-    res = requests.get(f"{BASE_URL}/index.php", params = dictionary_form)
-    
-    # Check i the script saved in the website:
-    if SCRIPT in res.text:
-        print("Your website is vulnerable to XSS Injection attack!")
-    else:
-        print("Your website is NOT vulnerable to XSS Injection attack.")
+        # Inserting the data and in put the script in the name filed:
+        res = sess.get(f"{BASE_URL}/index.php", params=dictionary_form)
 
-
+        # Check i the script saved in the website:
+        if SCRIPT in res.text:
+            print("Your website is vulnerable to XSS Injection attack!")
+        else:
+            print("Your website is NOT vulnerable to XSS Injection attack.")
 
 
 if __name__ == "__main__":
