234dcbda50a6d66c844c45b72515eff085715558
Appleboyav0 <avior2060@gmail.com>
2024-02-17 21:29:45 +0200

    removed return type from function signature

--- Helpers/helper_dvwa.py
+++ Helpers/helper_dvwa.py
@@ -1,12 +1,10 @@
 import requests
-from typing import Dict
-
 from Helpers import helper_generic_tags
 
 
 class DVWA:
     @staticmethod
-    def get_user_token(login_page_url) -> str:
+    def get_user_token(login_page_url: str) -> str:
         with requests.Session() as sess:
             res = sess.get(login_page_url)
             cookies = helper_generic_tags.GetGenericTags.get_tags(res.text, "input", {"type": "hidden"})
@@ -15,7 +13,7 @@ class DVWA:
         return user_token
 
     @staticmethod
-    def get_login_cookies(login_page_url: str, level: int = 0) -> Dict[str, str]:
+    def get_login_cookies(login_page_url: str, level: int = 0):
         security_level = ["low", "medium", "high", "impossible"]
 
         login_cookies = {
