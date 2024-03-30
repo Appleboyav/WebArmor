b4b09ebaeb4b35458ff0026a14fa0f42e275b3b2
Appleboyav0 <avior2060@gmail.com>
2024-03-31 00:30:43 +0300

    Changed the file structure a little bit. still need more work to be finished

--- CSRF/csrf_new.py
+++ CSRF/csrf_new.py
@@ -4,7 +4,7 @@ from Helpers import helper_generic_tags
 from Attack import base_attack
 from Attack import test_attack
 
-URL = "https://exampasdasdle.com/"
+URL = "https://example.com/"
 
 """
 1. לזהות redirect ומילים נפוצות של דפי התחברות
@@ -12,6 +12,7 @@ URL = "https://exampasdasdle.com/"
 3. להתחבר לאתר עם הפרטים שהוכנסו
 4. לעשות redirect בחזרה לכתובת הראשונית
 """
+# test123
 
 
 class CSRF(base_attack.Attack):
@@ -19,29 +20,33 @@ class CSRF(base_attack.Attack):
         super().__init__(url)
 
     def scan(self):
-        # # sess = requests.Session()
-        #
-        # # http://127.0.0.1:80/DVWA/login.php
-        # login_page_url = input("Enter login page url (to bypass it): ")
-        # user_token = helper_dvwa.DVWA.get_user_token(login_page_url)
-        #
-        # cookies_json = {
+        # region SCAN
+
+        cookies_dict = {
+            "security": "low"
+        }
+
+        # cookies_dict = {
         #     # "username": "admin",
         #     # "password": "password",
         #     # "Login": "Login",
         #     "PHPSESSID": user_token,
         #     "security": "low"
         # }
-        # print(cookies_json)
-        #
-        # with requests.Session() as sess:
-        #     sess.cookies.update(cookies_json)
-        #
-        #     # http://127.0.0.1:80/DVWA/vulnerabilities/csrf/
-        #     # http://127.0.0.1:80/DVWA/vulnerabilities/sqli/
-        #     csrf_url_page = input("Please enter a website url to check CSRF: ")
-        #     csrf_page_res = sess.get(csrf_url_page)
-        #     print(csrf_page_res.text)
+
+        with requests.Session() as sess:
+            sess.cookies.update(cookies_dict)
+
+            # http://127.0.0.1:80/DVWA/login.php
+            user_token = helper_dvwa.DVWA.get_user_token(self.url)
+
+
+            # http://127.0.0.1:80/DVWA/vulnerabilities/csrf/
+            csrf_url_page = input("Please enter a website url to check CSRF: ")
+            csrf_page_res = sess.get(csrf_url_page)
+            print(csrf_page_res.text)
+        # endregion
+
         print(f"Scanning from function: '{CSRF.scan.__name__}'\nClass: {self.__class__.__name__}\nUrl:'{self.url}'\n")
         self.test()
 
