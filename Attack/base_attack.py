1229698b7588a78ef2b82431cf67d099deea4898
Appleboyav0 <avior2060@gmail.com>
2024-04-07 01:21:40 +0300

    added test-attacks foldeer changes

--- Attack/base_attack.py
+++ Attack/base_attack.py
@@ -1,11 +1,8 @@
 
-
 class Attack:
-    def __init__(self, url):
+    def __init__(self, url: str):
         self.url = url
 
     def scan(self):
         print(f"Scanning from function: '{Attack.scan.__name__}'\nClass: {self.__class__.__name__}\nUrl:'{self.url}'\n")
-
         # pass
-
--- Test-Attacks/main.py
+++ Test-Attacks/main.py
@@ -1,7 +1,11 @@
+from enum import Enum
+import pyfiglet_ascii_art
+import validators
+
 from CSRF import csrf_new
 from SQLi.ErrorBased import error_based_sqli
-import pyfiglet_ascii_art
-from enum import Enum
+from BruteForce import brute_force_feature
+from XSS import xss
 
 
 class Attack_Choice(Enum):
@@ -23,12 +27,12 @@ def menu() -> None:
 
 
 def get_user_choice() -> int:
-    lowest_value = (min(Attack_Choice, key=lambda x: x.value)).value
-    highest_value = (max(Attack_Choice, key=lambda x: x.value)).value
+    lowest_value: int = (min(Attack_Choice, key=lambda x: x.value)).value
+    highest_value: int = (max(Attack_Choice, key=lambda x: x.value)).value
 
     while True:
         try:
-            user_input = int(input("Please choose one of the options above: "))
+            user_input: int = int(input("Please choose one of the options above: "))
 
             if lowest_value <= user_input <= highest_value:
                 return user_input
@@ -39,17 +43,34 @@ def get_user_choice() -> int:
             print("Invalid input, please enter a valid integer.")
 
 
+def validate_url() -> str:
+    while True:
+        url_to_scan: str = input("Please enter the url to scan:\n")
+
+        if validators.url(url_to_scan):
+            return url_to_scan
+        else:
+            print("Invalid URL. Please try again.")
+
+
 def main():
     welcome_screen()
     menu()
-    user_choice = get_user_choice()
+    user_choice: int = get_user_choice()
     print(user_choice)
 
-    # Create a list of attack objects and based on the user choice use that object and run it's scan function
-    attack_obj = [error_based_sqli.SQLi, csrf_new.CSRF]
-    attack = attack_obj[user_choice-1]("http://127.0.0.1/DVWA/vulnerabilities/sqli/")  # Create a function to get url from user.
+    # http://127.0.0.1/DVWA/vulnerabilities/sqli/  -> sqli
+    # http://127.0.0.1/DVWA/vulnerabilities/csrf/  -> csrf
+    # http://127.0.0.1/DVWA/                       -> xss
+    # http://127.0.0.1/DVWA/vulnerabilities/brute/ -> brute
+    url_to_scan: str = validate_url()
 
-    attack.scan()
+    # Create a list of attack objects and based on the user choice use that object and run it's scan function
+    attacks_obj_list = [error_based_sqli.SQLi, csrf_new.CSRF, xss.XSS, brute_force_feature.BruteForce]
+    attack_choice = attacks_obj_list[user_choice-1](url_to_scan)  # Create a function to get url from user.
+
+    # Run scan attack
+    attack_choice.scan()
 
 
 if __name__ == "__main__":
